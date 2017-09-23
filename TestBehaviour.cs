f586d4cce697ab3823b229db834f66d4ceb57541
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2017-09-23 13:33:34 -0300

    v2
    v2

---
+++ TestBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+}
+
+public class TestBehaviour : MonoBehaviour
+{
+
+	[PotionSelectorAttribute]
+	public string potionName;
+	Potion[] here;
+
+	public void Start()
+	{
+		//here = PotionDB.instance.potions;
+		Debug.Log(potionName);
+		for(int a = 0; a < PotionDB.instance.potions.Length; a ++){
+			if(potionName == PotionDB.instance.potions[a].name){
+			Debug.Log(PotionDB.instance.potions[a].heal);
+			}
+
+		}
+
+
+	}
+
+}
