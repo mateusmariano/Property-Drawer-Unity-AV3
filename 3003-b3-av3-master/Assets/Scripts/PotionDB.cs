5d170faab05389b38323d77646b904131cef9405
Mateus Mariano(buzz) <mtdmtdmtd3@gmail.com>
2017-09-23 12:20:29 -0300

    v1
    v1

---
+++ 3003-b3-av3-master/Assets/Scripts/PotionDB.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PotionDB : MonoBehaviour
+{
+	public static PotionDB Instance
+	{
+		get
+		{
+			if( instance == null )
+			{
+				instance = Resources.Load<PotionDB>("PotionDB");
+			}
+
+			return instance;
+		}
+	}
+
+	private static PotionDB instance;
+
+	public Potion[] potions;
+	public static Potion[] potionaux;
+
+	void Start(){
+		potionaux = potions;
+	}
+}
---
+++ 3003-b3-av3-master/Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+}
+
+public class TestBehaviour : MonoBehaviour
+{
+
+	[PotionSelector]
+	public string potionName;
+	public int numerodapocao;
+	 Potion[] here;
+
+	public void Start()
+	{
+		here = PotionDB.potionaux;
+		if(numerodapocao < here.Length){
+		potionName = here[numerodapocao].name;
+		Debug.Log("A poçao "+ potionName+ " recupoera: " + here[numerodapocao].heal);
+		}else{
+			Debug.Log("voce nao tem tantas poccoes");
+		}
+
+			
+
+
+		// PotionDB.Instance pra acessar o "banco de dados" de pocoes
+		// Imprimir o quanto aquela potion recupera
+	}
+
+}
